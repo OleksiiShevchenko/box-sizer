8c7cde2e631ac408cd96befb2729acc587922318
Oleksii Shevchenko <oleksii@Aleksejs-MBP-2.attlocal.net>
2026-03-31 17:07:42 -0700

    Add blue box favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Packwell - Find the Perfect Shipping Box",
   description: "Determine the right box size for your products",
+  icons: {
+    icon: "/email/packwell-mark.svg",
+    shortcut: "/email/packwell-mark.svg",
+    apple: "/email/packwell-mark.svg",
+  },
 };
 
 export default function RootLayout({
